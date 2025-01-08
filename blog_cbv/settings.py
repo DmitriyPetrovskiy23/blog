518c18775479a56d3d7143ed353a2546ed56a001
Dmitriy Petrovskiy <d.petrovskiy@yandex.ru>
2025-01-08 19:59:35 +0300

    Тестирование веток2

--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru'
-TIME_ZONE = 'Europe/Moscow123'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
